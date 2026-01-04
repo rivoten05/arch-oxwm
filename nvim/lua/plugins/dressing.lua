3dc0b645bb01384773bf3e77f26b172181f00862
rivo <rivo@gmail.com>
2026-01-04 19:38:41 +0530

    add dressing pluguns

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,4 +1,18 @@
 return {
 	"stevearc/dressing.nvim",
-	opts = {},
+	opts = {
+		border = "rounded",
+		mappings = {
+			n = {
+				["<Esc>"] = "Close",
+				["<CR>"] = "Confirm",
+			},
+			i = {
+				["<C-c>"] = "Close",
+				["<CR>"] = "Confirm",
+				["<Up>"] = "HistoryPrev",
+				["<Down>"] = "HistoryNext",
+			},
+		},
+	},
 }
